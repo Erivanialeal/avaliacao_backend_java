431ca392d234f16120f9287b0cea6d33e62e84a6
Erivania Leal <erivanial.s1507@gmail.com>
2026-08-20 19:48:28 -0300

    primeiro commit configuraçoes

---
+++ beneficiarios-api/src/main/java/br/com/erivania/beneficiarios_api/BeneficiariosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.erivania.beneficiarios_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiariosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficiariosApiApplication.class, args);
+	}
+
+}
---
+++ beneficiarios-api/src/test/java/br/com/erivania/beneficiarios_api/BeneficiariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erivania.beneficiarios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneficiariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
